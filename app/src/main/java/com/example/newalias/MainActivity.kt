8490fa4b1d77e3389d2fe457f8759f3ae5e4fad1
VitaliyKoshkin <kosh98vitaliy@outlook.com>
2022-02-07 15:47:26 +0300

    work with fragments

--- app/src/main/java/com/example/newalias/MainActivity.kt
+++ app/src/main/java/com/example/newalias/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.newalias
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newalias.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    fun OnStart() {
+        super.onStart()
+    }
+
+    fun OnResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+
     }
 }
